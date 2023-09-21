db515d1d6b559b1889b0fdc9faf9b840ed427659
“Nivin <getnivin@gmail.com>
2023-09-21 22:40:26 +0530

    HashMap - 242, 49

---
+++ HashMap/219-ContainsDuplicateII.java
@@ -0,0 +1,14 @@
+package HashMap;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            Integer lastIndex = map.put(nums[i], i);
+            if(lastIndex != null && (i - lastIndex) <= k){
+                return true;
+            }
+        }
+        return false;
+    }
+}
